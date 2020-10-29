c886c5eef52a4a617b3e68a344a2591c752267df
kudos07 <58479988+kudos07@users.noreply.github.com>
2020-10-29 18:13:27 +0530

    Add files via upload

---
+++ computeButton.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loanassistant;
+
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author 91912
+ */
+class computeButton {
+
+    static void setText(String compute_Monthly_Payment) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void addActionListener(ActionListener actionListener) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
---
+++ newLoanButton.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loanassistant;
+
+/**
+ *
+ * @author 91912
+ */
+class newLoanButton {
+    
+}
